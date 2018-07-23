ca6cec77dd4a11c512fdaefa57d9f921ca8d0e11
Ivan Luo <ivan@dotdev.com.au>
2018-07-23 11:14:31 +1000

    :bookmark: new features read email from excel and delete customers from shopify

---
+++ app/Console/Commands/DeleteShopifyCustomers.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Support\Facades\App;
+use Illuminate\Console\Command;
+use Shopify\PrivateApp;
+
+class DeleteShopifyCustomers extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'shopify:delete:customers';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Delete Shopify Customers';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $deleteIDs = [];
+        $idIndexer = 0;
+
+        $store = config('faceHalo.store');
+        $key = config('faceHalo.key');
+        $pass = config('faceHalo.pass');
+        $secret = config('faceHalo.secret');
+        $faceHelo = new PrivateApp($store,$key,$pass,$secret);
+
+        $excelEmails = $this->getExcelEmails();
+
+        foreach($excelEmails as $excelEmail){
+            try{
+                $response = $faceHelo->get('customers/search', ['query' => ['query' => 'email:'.$excelEmail['email']]]);
+                $deleteIDs[$idIndexer] = $response->customers[0]->id;
+                $idIndexer++;
+            }catch (\Exception $e){
+                $this->info('errors: get shopify customers');
+                continue;
+            }
+        }
+
+        foreach($deleteIDs as $deleteID){
+            try{
+                $faceHelo->delete('customers/'.$deleteID);
+                $this->info('customer'.$deleteID.'deleted');
+            }catch (\Exception $e){
+                $this->info('errors: Error deleting customer');
+                continue;
+            }
+
+
+        }
+
+    }
+
+    public function getExcelEmails(){
+        $excel = App::make('excel');
+        $filePath = '/Users/dotdev/Desktop/test.xlsx';
+        $excelEmails = $excel->load($filePath, function ($reader){
+            $data = $reader->select(array('email'))->get();
+            $collection = collect($data);
+            $flattened = $collection->flatten();
+            return $flattened->all();
+        })->toArray();
+        return $excelEmails;
+    }
+}
