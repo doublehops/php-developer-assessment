5dfb7051b02d9d6770cba769c3365c4ee7a2b4e2
Ivan Luo <ivan@dotdev.com.au>
2018-07-23 12:20:42 +1000

    :bookmark: new features read email from excel and delete customers from shopify

---
+++ config/faceHalo.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dotdev
+ * Date: 20/7/18
+ * Time: 11:16 AM
+ */
+
+return[
+    "store" => env('SHOPIFY_STORE', ''),
+    "key" => env('SHOPIFY_KEY', ''),
+    "pass" => env('SHOPIFY_PASSWORD', ''),
+    "secret" => env('SHOPIFY_SECRET', ''),
+    "app_secret" => env('APP_SECRET', ''),
+];
